84aded2233ca6d97734324f543a53b22b409fdb2
Guada <guadapdls@gmail.com>
2023-05-19 20:45:37 -0300

    ajustes

--- src/services/requestsArtist.js
+++ src/services/requestsArtist.js
@@ -3,10 +3,9 @@ import requestsAccess from './requestsAccess'
 export async function searchArtists (searchInput) {
   try {
     const searchParameters = await requestsAccess()
-    console.log(searchParameters)
-    const artists = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', {...searchParameters})
+    const artists = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => data.artists.items)
     return artists
   } catch (error) {
     console.log(error)
