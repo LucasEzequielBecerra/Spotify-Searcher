b490b3c3618208206d598fee6a0eaeb78a4c5842
Lucas Becerra <lucaseramos13@gmail.com>
2023-05-19 21:09:30 -0300

    c to p

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
 import requestsArtist from '../../services/requestsArtist'
-import { Input, Card, CardCover, CardContent, Typography } from '@mui/joy'
-import { Search } from '@mui/icons-material'
 
 const Home = async () => {
-  const [searchInput, setSearchInput] = useState('')
-  const searchSongs = await requestsArtist(searchInput)
-
   return (
     <>
     <div>Home</div>
-    <Input onKeyDown={ async e => { if (e.key === 'Enter') { await searchSongs() } }} onChange={e => setSearchInput(e.target.value)} value={searchInput} placeholder='Search' startDecorator={<Search />}>Searcher</Input>
+
     <div className='containerCards'></div>
     </>
   )
--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -2,11 +2,11 @@ import { Input, Card, CardCover, CardContent, Typography } from '@mui/joy'
 import { Search } from '@mui/icons-material'
 
 const Searcher = () => {
-  function handleChange () {
+  function handleChange (e) {
 
   }
   return (
-    <Input placeholder='Search' startDecorator={<Search />}>Searcher</Input>
+    <Input onChange={e => handleChange(e)} placeholder='Search' startDecorator={<Search />}>Searcher</Input>
   )
 }
 
