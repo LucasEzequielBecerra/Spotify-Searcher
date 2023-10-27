7196a40c214530e87fc759344dba02bcc132e60f
Guada <110698501+Guada-pdls@users.noreply.github.com>
2023-10-26 21:06:21 -0300

    Guada branch (#14)
    * Estilos
    
    * correcciones y load
    
    * correction ItemCardContainer component
    
    * router
    
    * delete mui
    
    * HomeHeader
    
    * loader, requestCategories, route categories & responsive
    
    * search input

---
+++ src/services/searchtItems.js
@@ -0,0 +1,20 @@
+import requestsAccess from './requestAccess'
+
+export async function searchItems (searchInput, categories) {
+  // Declaro el tipo de búsqueda y su límite
+  if (categories.length === 0) categories = ['track', 'artist', 'album', 'playlist', 'episode']
+  const type = '&type=' + categories.join('%2C')
+  const limit = categories.length === 1 ? 50 : 12
+
+  // Genero la url y realizo la petición
+  try {
+    const searchParameters = await requestsAccess()
+    if (searchInput === '') return []
+    const data = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + type + `&limit=${limit}`, searchParameters)
+      .then(response => response.json())
+      .then(data => data)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
