df57da2ecc052d72ff83922ed84d13625347da05
Lucas Becerra <lucaseramos13@gmail.com>
2023-10-30 21:29:41 -0300

    implemento albums cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CategoriesListContainer from '../Categories/CategoriesListContainer'
 import HomeHeader from './HomeHeader'
 import ResultsScreen from '../ResultsViews/ResultsScreen'
@@ -7,15 +7,12 @@ const Home = () => {
   const [loading, setLoading] = useState(true)
   const [searchResults, setSearchResults] = useState(['a'])
 
-  console.log(searchResults)
-
   return (
     <>
       <HomeHeader setSearchResults={setSearchResults} setLoading={setLoading} />
       {searchResults.length === 0
         ? (<CategoriesListContainer loading={loading} setLoading={setLoading} />)
         : (<ResultsScreen />)
-
       }
     </>
   )
