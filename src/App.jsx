e6f53fb12b22f560894f5d3b9d13d167e1caae8e
Lucas Becerra <lucaseramos13@gmail.com>
2023-12-30 16:40:49 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 import Home from './components/Home/Home'
-=======
-import Home from './Components/Home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { SearchProvider } from './context/SearchContext'
 // import GenreDetails from './components/Details/GenreDetails'
->>>>>>> 3431c8139f32c87d6442f04b52be923737dc10e2
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
       <Home />
-=======
       <SearchProvider>
         <BrowserRouter>
           <Routes>
@@ -21,7 +15,6 @@ const App = () => {
           </Routes>
         </BrowserRouter>
       </SearchProvider>
->>>>>>> 3431c8139f32c87d6442f04b52be923737dc10e2
     </>
   )
 }
